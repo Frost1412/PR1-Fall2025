d7c0acd0a8c8f5eac865bb0f62162927f6cdceb7
Fr0sTByte12 <Nguyenkimdinh1412@gmail.com>
2025-09-15 01:27:41 +0700

    first commit

---
+++ test.java
@@ -0,0 +1,9 @@
+package PR1_Ver2;
+
+public class test {
+    public static void main(String[] args) {
+       String a = "Chao mung moi nguoi den voi lop PR1!";
+        System.out.println(a);
+
+    }
+}

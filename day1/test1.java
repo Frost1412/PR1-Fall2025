e177586c7b913b75fc1f0115f4aad9d89ed27053
Fr0sTByte12 <Nguyenkimdinh1412@gmail.com>
2025-09-15 01:31:19 +0700

    update 1.0

---
+++ day1/test1.java
@@ -0,0 +1,15 @@
+package PR1_Ver2.day1;
+
+import java.util.Scanner;
+
+
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println(c);
+    }
+}

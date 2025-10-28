a6f7b7407c80e1d851c17f42d9f0364e94da19ef
Fr0sTByte12 <Nguyenkimdinh1412@gmail.com>
2025-10-28 23:33:56 +0700

    sort&search

---
+++ day5/selection_sort.java
@@ -0,0 +1,54 @@
+package PR1_Ver2.day5;
+
+import java.util.Arrays;
+
+public class selection_sort {
+
+    static void selectionSort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+
+            // Assume the current position holds
+            // the minimum element
+            int min_idx = i;
+
+            // Iterate through the unsorted portion
+            // to find the actual minimum
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+
+                    // Update min_idx if a smaller element
+                    // is found
+                    min_idx = j;
+                }
+            }
+
+            // Move minimum element to its
+            // correct position
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+
+    static void printArray(int[] arr){
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int[] arr = { 41, 14, 12, 7, 11 };
+
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array: ");
+        printArray(arr);
+    }
+}
+
+// https://www.youtube.com/shorts/OszAf8ZK7Is (Xem vid nay la hieu ngay van de)
